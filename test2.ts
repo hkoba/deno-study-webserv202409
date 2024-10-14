5556a5768443a2d4efcbc68ff259ff7721300a80
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-10-14 15:03:45 +0900

    これは foobar が export されずエラーに。何事？

--- test2.ts
+++ test2.ts
@@ -1,11 +1,28 @@
 async function test() {
   const mod = await import(`data:text/javascript,
-  function foobar(s) {
-    return s + s;
-  }
-  export {
-    foobar
-  };
+// test1.ts
+function foobar(s) {
+  return s + s;
+}
+
+// main.ts
+function add(a, b) {
+  return a + b;
+}
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("foobar: ", foobar("foo"));
+}
+
+// dynamic-content.ts
+function foobar2(s) {
+  console.log("foobar<br>\n");
+  console.log("add: ", add(2, 3));
+  return s;
+}
+export {
+  foobar2 as foobar
+};
 `)
 
   console.log(mod)
