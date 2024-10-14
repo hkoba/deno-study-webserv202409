874cde494b12be4068b0a63fe4d0c408ef41a865
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-10-14 15:03:23 +0900

    これは通る

---
+++ test2.ts
@@ -0,0 +1,15 @@
+async function test() {
+  const mod = await import(`data:text/javascript,
+  function foobar(s) {
+    return s + s;
+  }
+  export {
+    foobar
+  };
+`)
+
+  console.log(mod)
+  console.log(mod.foobar("baz"))
+}
+
+await test()
